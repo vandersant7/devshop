63541e71d8247f9830e215c25dda9b66747f2c79
Evandro Santos <vandersant7@gmail.com>
2024-05-26 17:22:05 -0300

    feat: create Products section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import Banner from "./components/Banner/Banner"
 import Category from "./components/Category/Category"
 import Category2 from "./components/Category/Category2"
 import Hero from "./components/Hero/Hero"
 import NavBar from "./components/NavBar/NavBar"
 import Services from "./components/Services/Services"
+
+import headphone from "./assets/hero/headphone.png"
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4:
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil repellat nemo perspiciatis necessitatibus.",
+  bgColor: "#f42c37"
+}
 
 
 function App() {
@@ -15,6 +30,8 @@ function App() {
       <Category />
       <Category2 />
       <Services />
+      <Banner data={BannerData} />
+      <Products />
     </div>
   )
 }
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,13 @@
+const Products = () => {
+    return ( 
+        <div>
+            <div className="container">
+                {/* header section  */}
+                
+                {/* body section  */}
+            </div>
+        </div>
+     );
+}
+ 
+export default Products; 
