d5aa389d91eb3ed78466ca940b6a20faa71feca1
Evandro Santos <vandersant7@gmail.com>
2024-05-26 19:17:11 -0300

    styles: products section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,13 +1,89 @@
+import Heading from '../Shared/Heading'
+import ProductCard from './ProductCard'
+
+import Img1 from "../../assets/product/p-1.jpg"
+import Img2 from "../../assets/product/p-2.jpg"
+import Img3 from "../../assets/product/p-3.jpg"
+import Img4 from "../../assets/product/p-4.jpg"
+// import Img5 from "../../assets/product/p-5.jpg"
+// import Img6 from "../../assets/product/p-6.jpg"
+// import Img7 from "../../assets/product/p-7.jpg"
+
+const ProductsData = [
+    {
+        id: 1,
+        img: Img1,
+        title: "Boat Headphone",
+        price: "120",
+        aosDalay: "0",
+    },
+    {
+        id: 2,
+        img: Img2,
+        title: "Rocky Mountain",
+        price: "420",
+        aosDalay: "200",
+    },
+    {
+        id: 3,
+        img: Img3,
+        title: "Goggles",
+        price: "320",
+        aosDalay: "400",
+    },
+    {
+        id: 4,
+        img: Img4,
+        title: "Printed",
+        price: "220",
+        aosDalay: "600",
+    },
+]
+
+const ProductsData2 = [
+    {
+        id: 1,
+        img: Img1,
+        title: "Boat Headphone",
+        price: "120",
+        aosDalay: "0",
+    },
+    {
+        id: 2,
+        img: Img2,
+        title: "Rocky Mountain",
+        price: "420",
+        aosDalay: "200",
+    },
+    {
+        id: 3,
+        img: Img3,
+        title: "Goggles",
+        price: "320",
+        aosDalay: "400",
+    },
+    {
+        id: 4,
+        img: Img4,
+        title: "Printed",
+        price: "220",
+        aosDalay: "600",
+    },
+]
+
 const Products = () => {
-    return ( 
-        <div>
-            <div className="container">
-                {/* header section  */}
-                
-                {/* body section  */}
-            </div>
-        </div>
-     );
+  return (
+    <div>
+      <div className="container">
+        {/* header section  */}
+        <Heading title="Our Products" subtitle={'Explore Our Products'} />
+
+        {/* body section  */}
+        <ProductCard data={ProductsData} />
+        <ProductCard data={ProductsData2} />
+      </div>
+    </div>
+  )
 }
- 
-export default Products; 
+
+export default Products
