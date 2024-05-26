c9c0aca59b35ac0442913ded8351c995643513e6
Evandro Santos <vandersant7@gmail.com>
2024-05-26 19:16:05 -0300

    feat: add Heading component

--- src/components/Shared/Heading.jsx
+++ src/components/Shared/Heading.jsx
@@ -1,8 +1,8 @@
 const Heading = ({ title, subtitle }) => {
   return (
-    <div>
-      <h1> {title} </h1>
-      <p> {subtitle} </p>
+    <div className="text-center mb-10 max-w-[600px] mx-auto space-y-2 ">
+      <h1 className="text-3xl font-bold lg:text-4xl"> {title} </h1>
+      <p className="text-xs text-gray-400"> {subtitle} </p>
     </div>
   )
 }
